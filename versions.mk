18871207c9b2fe63a1079dbe5725a2cd0d28b3f6
Alexey Sheplyakov <asheplyakov@altlinux.org>
2019-11-25 08:23:35 +0400

    Query CLN and GiNaC versions from git submodules

--- versions.mk
+++ versions.mk
@@ -1,4 +1,4 @@
 
 gmp_VERSION := 5.1.3
-cln_VERSION := 1.3.4+20191107+git1a03695164eb
-ginac_VERSION := 1.7.7
+cln_VERSION := $(shell git --git-dir=cln/.git describe --tags | sed -rne 's/^cln_([0-9]+)[-]([0-9]+)[-]([0-9]+)(.*)$$/\1.\2.\3\4/p')
+ginac_VERSION := $(shell git --git-dir=ginac/.git describe --tags | sed -rne 's/^release_([0-9]+)[-]([0-9]+)[-]([0-9]+)(.*)$$/\1.\2.\3\4/p')
