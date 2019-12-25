dc340f2c1fe771de7d68500f10fd7215ac8d3ae9
Alexey Sheplyakov <asheplyakov@altlinux.org>
2019-12-25 20:48:21 +0400

    Use CMake'ified CLN and GiNaC

---
+++ mk/i686-w64-mingw32-toolchain.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(_arch "i686-w64-mingw32")
+#set(_arch "x86_64-w64-mingw32")
+
+set(CMAKE_C_COMPILER   ${_arch}-gcc)
+set(CMAKE_CXX_COMPILER ${_arch}-g++)
+set(CMAKE_RC_COMPILER  ${_arch}-windres)
+
+set(CMAKE_FIND_ROOT_PATH /usr/${_arch};${USER_FIND_ROOT_PATH})
+# Don't search for executables under CMAKE_FIND_ROOT_PATH
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# Search for host libraries under CMAKE_FIND_ROOT_PATH only
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+# Search for host headers under CMAKE_FIND_ROOT_PATH only
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
