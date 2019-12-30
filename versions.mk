09e44641c17ce2ec4d39e0e5136becab9e78baad
Alexey Sheplyakov <asheplyakov@altlinux.org>
2019-12-30 12:00:15 +0400

    Updated to GMP 6.1.2

--- versions.mk
+++ versions.mk
@@ -1,4 +1,4 @@
 
-gmp_VERSION := 5.1.3
+gmp_VERSION := $(shell git --git-dir=gmp/.git describe --tags | sed -rne 's/^v([^ ]+)/\1/p')
 cln_VERSION := $(shell git --git-dir=cln/.git describe --tags | sed -rne 's/^cln_([0-9]+)[-]([0-9]+)[-]([0-9]+)(.*)$$/\1.\2.\3\4/p')
 ginac_VERSION := $(shell git --git-dir=ginac/.git describe --tags | sed -rne 's/^release_([0-9]+)[-]([0-9]+)[-]([0-9]+)(.*)$$/\1.\2.\3\4/p')
